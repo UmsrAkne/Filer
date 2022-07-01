6d320c4601f37c7eec57f6542bff296608c65dde
Akn <sibata486@gmail.com>
2022-07-01 21:18:32 +0900

    add / ファイル、またはディレクトリを表すクラスを追加

---
+++ Filer/Models/ExtendFileInfo.cs
@@ -0,0 +1,37 @@
+﻿namespace Filer.Models
+{
+    using System.IO;
+    using Prism.Mvvm;
+
+    public class ExtendFileInfo : BindableBase
+    {
+        private bool isSelected;
+        private int index;
+        private bool marked;
+
+        public ExtendFileInfo(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                FileSystemInfo = new DirectoryInfo(path);
+                IsDirectory = true;
+            }
+            else
+            {
+                FileSystemInfo = new FileInfo(path);
+            }
+        }
+
+        public FileSystemInfo FileSystemInfo { get; private set; }
+
+        public string Name => FileSystemInfo.Name;
+
+        public bool IsDirectory { get; }
+
+        public bool IsSelected { get => isSelected; set => SetProperty(ref isSelected, value); }
+
+        public int Index { get => index; set => SetProperty(ref index, value); }
+
+        public bool Marked { get => marked; set => SetProperty(ref marked, value); }
+    }
+}
