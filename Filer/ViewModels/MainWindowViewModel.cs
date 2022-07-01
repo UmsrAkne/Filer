1c8fa51cbbdb1e34246ba4657aa2e0d935b9c6da
Akn <sibata486@gmail.com>
2022-07-01 21:48:53 +0900

    add / ファイルをビューに表示

--- Filer/ViewModels/MainWindowViewModel.cs
+++ Filer/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,44 @@
 ﻿namespace Filer.ViewModels
 {
+    using System;
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Linq;
+    using Filer.Models;
     using Prism.Mvvm;
 
     public class MainWindowViewModel : BindableBase
     {
         private string title = "Prism Application";
 
+        private ObservableCollection<ExtendFileInfo> leftFileList = new ObservableCollection<ExtendFileInfo>();
+        private ObservableCollection<ExtendFileInfo> rightFileList = new ObservableCollection<ExtendFileInfo>();
+
         public MainWindowViewModel()
         {
+            LeftFileList = GetFileList(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+            RightFileList = GetFileList(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
         }
 
         public string Title
         {
             get { return title; }
             set { SetProperty(ref title, value); }
+        }
+
+        public ObservableCollection<ExtendFileInfo> LeftFileList { get => leftFileList; set => SetProperty(ref leftFileList, value); }
+
+        public ObservableCollection<ExtendFileInfo> RightFileList { get => rightFileList; set => SetProperty(ref rightFileList, value); }
+
+        private ObservableCollection<ExtendFileInfo> GetFileList(string path)
+        {
+            var defaultDirectoryInfo = new DirectoryInfo(path);
+            var directories = defaultDirectoryInfo.GetDirectories().Select(d => new ExtendFileInfo(d.FullName));
+            var files = defaultDirectoryInfo.GetFiles().Select(f => new ExtendFileInfo(f.FullName));
+
+            var bothList = directories.Concat(files);
+
+            return new ObservableCollection<ExtendFileInfo>(directories.Concat(files));
         }
     }
 }
