8488a38522c334360af652353ee1e59fa713d3c2
Akn <sibata486@gmail.com>
2022-07-07 19:22:54 +0900

    add / 当該機能に必要なビューとビューモデルを追加

---
+++ Filer/ViewModels/SelectionDialogViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Filer.ViewModels
+{
+    using System;
+    using Prism.Services.Dialogs;
+
+    public class SelectionDialogViewModel : IDialogAware
+    {
+        public string Title => "SelectionDialog";
+
+        public event Action<IDialogResult> RequestClose;
+
+        public bool CanCloseDialog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDialogClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
