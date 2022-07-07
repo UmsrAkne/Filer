c44e52a8c71762ad554a90efdb485216642bef94
Akn <sibata486@gmail.com>
2022-07-07 20:53:12 +0900

    wip / ファイル、フォルダの作成機能の実装中

---
+++ Filer/Models/SelectionListItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Filer.Models
+{
+    using System.Windows.Input;
+
+    public class SelectionListItem
+    {
+        public Key Key { get; set; }
+
+        public string KeyText => Key.ToString();
+
+        public string Header { get; set; }
+
+        public int Index { get; set; }
+    }
+}
--- Filer/ViewModels/SelectionDialogViewModel.cs
+++ Filer/ViewModels/SelectionDialogViewModel.cs
@@ -1,15 +1,34 @@
 ﻿namespace Filer.ViewModels
 {
     using System;
+    using System.Collections.Generic;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using Filer.Models;
     using Prism.Commands;
+    using Prism.Mvvm;
     using Prism.Services.Dialogs;
 
-    public class SelectionDialogViewModel : IDialogAware
+    public class SelectionDialogViewModel : BindableBase, IDialogAware
     {
         private DelegateCommand confirmCommand;
         private DelegateCommand cancelCommand;
+        private DelegateCommand<TextBox> createDirectoryCommand;
+        private DelegateCommand<TextBox> createFileCommand;
+        private int selectedIndex = 0;
+        private string inputName = "defaultName";
 
         public event Action<IDialogResult> RequestClose;
+
+        public List<SelectionListItem> SelectableItems { get; set; } = new List<SelectionListItem>()
+        {
+            new SelectionListItem() { Header = "Directory", Key = Key.D, Index = 1 },
+            new SelectionListItem() { Header = "Empty File", Key = Key.F, Index = 2 },
+        };
+
+        public string InputName { get => inputName; set => SetProperty(ref inputName, value); }
+
+        public int SelectedIndex { get => selectedIndex; set => SetProperty(ref selectedIndex, value); }
 
         public string Title => "SelectionDialog";
 
@@ -26,6 +45,24 @@
             get => cancelCommand ?? (cancelCommand = new DelegateCommand(() =>
             {
                 RequestClose?.Invoke(new DialogResult());
+            }));
+        }
+
+        public DelegateCommand<TextBox> CreateDirectoryCommand
+        {
+            get => createDirectoryCommand ?? (createDirectoryCommand = new DelegateCommand<TextBox>((tb) =>
+            {
+                SelectedIndex = 0;
+                Keyboard.Focus(tb);
+            }));
+        }
+
+        public DelegateCommand<TextBox> CreateFileCommand
+        {
+            get => createFileCommand ?? (createFileCommand = new DelegateCommand<TextBox>((tb) =>
+            {
+                SelectedIndex = 1;
+                Keyboard.Focus(tb);
             }));
         }
 
