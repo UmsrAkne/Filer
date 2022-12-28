2f23d31bfa5145b3bc3a6383bdd91a9a21e28030
Akn <sibata486@gmail.com>
2022-12-28 14:27:15 +0900

    add / 設定ファイルそのものと、設定ファイルの読み書きを行うクラスを追加

---
+++ Filer/Models/Settings/Favorite.cs
@@ -0,0 +1,11 @@
+namespace Filer.Models.Settings
+{
+    public class Favorite
+    {
+        public string Key { get; set; }
+
+        public string Path { get; set; }
+
+        public string Name { get; set; }
+    }
+}
