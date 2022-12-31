0a355dd3011ec7a9f8bda7dcb316999d205996c5
Akn <sibata486@gmail.com>
2022-12-31 14:34:57 +0900

    add / KeyValueListPage とそれに関連するファイルを追加

---
+++ Filer/Views/KeyValueListPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Filer.Views
+{
+    public partial class KeyValueListPage : Page
+    {
+        public KeyValueListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
