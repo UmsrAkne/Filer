eb91d54c35d58ce444529516704cc0d08b196a55
Akn <sibata486@gmail.com>
2023-01-01 01:46:15 +0900

    add, wip / KeyValueListPageViewModel への機能追加

--- Filer/ViewModels/KeyValueListPageViewModel.cs
+++ Filer/ViewModels/KeyValueListPageViewModel.cs
@@ -1,8 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Filer.Models.Settings;
+using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Services.Dialogs;
 
 namespace Filer.ViewModels
 {
-    public class KeyValueListPageViewModel : BindableBase
+    public class KeyValueListPageViewModel : BindableBase, IDialogAware
     {
+        private string keyText;
+
+        private DelegateCommand executeFromKeyCommand;
+
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title { get; } = string.Empty;
+
+        public string KeyText { get => keyText; set => SetProperty(ref keyText, value); }
+
+        public List<Favorite> Favorites { get; set; }
+
+        public DelegateCommand ExecuteFromKeyCommand =>
+            executeFromKeyCommand ?? (executeFromKeyCommand = new DelegateCommand(() =>
+            {
+            }));
+
+        public bool CanCloseDialog() => true;
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            var setting = ApplicationSetting.ReadApplicationSetting(ApplicationSetting.AppSettingFileName);
+            Favorites = setting.Favorites;
+        }
     }
 }
