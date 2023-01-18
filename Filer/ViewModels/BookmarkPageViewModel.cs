d1fdb6f8ba9779f3e3073660a10812eed3e026f5
Akn <sibata486@gmail.com>
2023-01-19 01:10:40 +0900

    add / ブックマークページのビューを作成
    具体的な処理部分はまだ未完成だが、見た目に関するコードは一通り終了

--- Filer/ViewModels/BookmarkPageViewModel.cs
+++ Filer/ViewModels/BookmarkPageViewModel.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.ObjectModel;
+using Filer.Models.Settings;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
 
@@ -7,21 +10,60 @@ namespace Filer.ViewModels
     public class BookmarkPageViewModel : BindableBase, IDialogAware
     {
         private string keyText;
+        private Mode mode;
+        private string buttonText;
 
         public event Action<IDialogResult> RequestClose;
 
+        public enum Mode
+        {
+            #pragma warning disable SA1602
+            AdditionMode,
+            JumpMode,
+            #pragma warning restore SA1602
+        }
+
         public string Title => "Bookmarks";
 
+        public string ButtonText
+        {
+            get => buttonText;
+            private set => SetProperty(ref buttonText, value);
+        }
+
         public string KeyText { get => keyText; set => SetProperty(ref keyText, value); }
+
+        public ObservableCollection<Favorite> Favorites { get; } = new ObservableCollection<Favorite>();
+
+        public DelegateCommand ButtonCommand { get; private set; }
 
         public bool CanCloseDialog() => true;
 
         public void OnDialogClosed()
         {
+            RequestClose?.Invoke(new DialogResult());
         }
 
         public void OnDialogOpened(IDialogParameters parameters)
         {
+            mode = parameters.GetValue<Mode>(nameof(Mode));
+
+            if (mode == Mode.AdditionMode)
+            {
+                ButtonText = "ブックマークを追加";
+                ButtonCommand = new DelegateCommand(() =>
+                {
+                    // ブックマークを追加する処理
+                });
+            }
+            else if (mode == Mode.JumpMode)
+            {
+                ButtonText = "ブックマークにジャンプ";
+                ButtonCommand = new DelegateCommand(() =>
+                {
+                    // ブックマークにジャンプする処理
+                });
+            }
         }
     }
 }
