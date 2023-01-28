5656de115ef1e38d441bf687e687c20d50447ad7
Akn <sibata486@gmail.com>
2023-01-29 00:14:52 +0900

    add / HistoryPage にコントロールを追加

--- Filer/ViewModels/HistoryPageViewModel.cs
+++ Filer/ViewModels/HistoryPageViewModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.ObjectModel;
+using Filer.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
@@ -7,9 +9,13 @@ namespace Filer.ViewModels
 {
     public class HistoryPageViewModel : BindableBase, IDialogAware
     {
+        private ObservableCollection<History> histories;
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
+
+        public ObservableCollection<History> Histories { get => histories; set => SetProperty(ref histories, value); }
 
         public DelegateCommand CloseCommand => new DelegateCommand(() =>
         {
