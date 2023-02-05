992b02151d94b11b52988a7002af693becd6b63c
Akn <sibata486@gmail.com>
2023-02-06 00:13:33 +0900

    add / InputPage とビューモデルのプロパティをバインディング

--- Filer/ViewModels/InputPageViewModel.cs
+++ Filer/ViewModels/InputPageViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
@@ -7,14 +8,31 @@ namespace Filer.ViewModels
 {
     public class InputPageViewModel : BindableBase, IDialogAware
     {
+        private string message;
+        private string inputText;
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
+
+        public string Message
+        {
+            get => message;
+            set => SetProperty(ref message, value);
+        }
+
+        public string InputText
+        {
+            get => inputText;
+            set => SetProperty(ref inputText, value);
+        }
 
         public DelegateCommand CloseCommand => new DelegateCommand(() =>
         {
             RequestClose?.Invoke(new DialogResult());
         });
+
+        private DirectoryInfo CurrentDirectoryInfo { get; set; }
 
         public bool CanCloseDialog() => true;
 
@@ -24,6 +42,8 @@ namespace Filer.ViewModels
 
         public void OnDialogOpened(IDialogParameters parameters)
         {
+            CurrentDirectoryInfo = parameters.GetValue<DirectoryInfo>(nameof(DirectoryInfo));
+            Message = parameters.GetValue<string>(nameof(Message));
         }
     }
 }
