2c0e31b3cd838825eea46f013f853cd8e9b50503
Akn <sibata486@gmail.com>
2023-02-06 00:27:36 +0900

    add / InputPage に、ファイルやディレクトリを作成する機能を実装

--- Filer/ViewModels/InputPageViewModel.cs
+++ Filer/ViewModels/InputPageViewModel.cs
@@ -6,10 +6,12 @@ using Prism.Services.Dialogs;
 
 namespace Filer.ViewModels
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class InputPageViewModel : BindableBase, IDialogAware
     {
         private string message;
         private string inputText;
+        private DelegateCommand confirmCommand;
 
         public event Action<IDialogResult> RequestClose;
 
@@ -32,7 +34,46 @@ namespace Filer.ViewModels
             RequestClose?.Invoke(new DialogResult());
         });
 
+        public DelegateCommand ConfirmCommand =>
+            confirmCommand ?? (confirmCommand = new DelegateCommand(() =>
+            {
+                var fileName = $@"{CurrentDirectoryInfo.FullName}\{InputText}";
+
+                // パスに無効な文字が含まれていないか
+                if (fileName.IndexOfAny(Path.GetInvalidPathChars()) >= 0)
+                {
+                    RequestClose?.Invoke(new DialogResult());
+                    return;
+                }
+
+                // 同名のファイルかディレクトリが存在していないか
+                if (Directory.Exists(fileName) || File.Exists(fileName))
+                {
+                    RequestClose?.Invoke(new DialogResult());
+                    return;
+                }
+
+                switch (FileSystemInfo)
+                {
+                    case DirectoryInfo _:
+                        Directory.CreateDirectory(fileName);
+                        break;
+
+                    case FileInfo _:
+                        File.Create(fileName).Close();
+                        break;
+                }
+
+                RequestClose?.Invoke(new DialogResult());
+            }));
+
         private DirectoryInfo CurrentDirectoryInfo { get; set; }
+
+        /// <summary>
+        /// ページ内で作成するファイルがディレクトリかファイルかを表すプロパティです
+        /// is FileInfo ならファイル is DirectoryInfo ならディレクトリです。
+        /// </summary>
+        private FileSystemInfo FileSystemInfo { get; set; }
 
         public bool CanCloseDialog() => true;
 
@@ -44,6 +85,12 @@ namespace Filer.ViewModels
         {
             CurrentDirectoryInfo = parameters.GetValue<DirectoryInfo>(nameof(DirectoryInfo));
             Message = parameters.GetValue<string>(nameof(Message));
+            FileSystemInfo = parameters.GetValue<FileSystemInfo>(nameof(FileSystemInfo));
+
+            if (FileSystemInfo == null || CurrentDirectoryInfo == null)
+            {
+                throw new ArgumentException("FileSystemInfo または CurrentDirectoryInfo のいずれかが null です。");
+            }
         }
     }
 }
